2d4168db22c36968b00fc996892f96a5825de3f8
GeorgesTokam <Georgestokam@live.fr>
2017-04-30 16:59:28 +0200

    Ajout d'activités

--- app/src/main/java/com/example/georges/tourapp/user.java
+++ app/src/main/java/com/example/georges/tourapp/user.java
@@ -17,28 +17,35 @@ public class user implements Serializable {
     private String    motDePasse;
 
     public int getId() {
+
         return id;
     }
     public void setId(int id) {
+
         this.id = id;
     }
     public String getNom() {
+
         return nom;
     }
     public void setNom(String nom) {
         this.nom = nom;
     }
     public String getEmail() {
+
         return email;
     }
     public void setEmail(String email) {
+
         this.email = email;
     }
 
     public String getMotDePasse() {
+
         return motDePasse;
     }
     public void setMotDePasse(String motDePasse) {
+
         this.motDePasse = motDePasse;
     }
 
